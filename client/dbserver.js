4d84a00e230e37c0f8b66e680297f5bf3ee9832e
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-17 16:35:22 +0530

    Update dbserver.js

--- client/dbserver.js
+++ client/dbserver.js
@@ -1,52 +1,7 @@
 
 
-// const express = require('express');
-// const { connectToDatabase } = require('./config/db.js');
-// const bodyParser = require('body-parser');
-// const Subscriber = require('./models/subscriber');
-// require('dotenv').config();
-// const cors = require('cors');
-
-// // Load environment variables
-// const PORT = 5000 ;
-// const uri = process.env.mongo_uri; 
-
-// uri;
-
-// // Initialize the Express application
-// const app = express();
-
-// // Middleware 
-// app.use(express.json());
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// connectToDatabase().then(() => {
-//     app.listen(5000, () => {
-//         console.log(`Server is running on port ${5000}`);
-//     });
-// });
-
-// // Subscription endpoint
-// app.post('/subscribe', async (req, res) => {
-//     const { email } = req.body;
-
-//     if (!email) {
-//         return res.status(400).json({ message: 'Email is required' });
-//     }
-
-//     try {
-//         const subscriber = new Subscriber({ email });
-//         await subscriber.save();
-//         res.status(200).json({ message: 'Subscription successful' });
-//     } catch (error) {
-//         console.error('Error subscribing:', error);
-//         res.status(500).json({ message: 'An error occurred while processing your request.' });
-//     }
-// });
-
-
 require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const { connectToDatabase } = require('./config/db');
 
@@ -90,7 +45,7 @@ app.post('/subscribe', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT||8000 ;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
