f25b34b7c071a9ce99021eb2f7a15b7bfde81140
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-19 00:07:29 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,12 @@ import crispReducer from './crispSlice';
 import {
   persistStore,
   persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
 } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 
