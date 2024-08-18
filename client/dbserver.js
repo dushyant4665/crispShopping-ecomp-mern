ed98a62a0842ac1daf3ce063df040cf3ec42a098
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-18 14:04:38 +0530

    Update dbserver.js

--- client/dbserver.js
+++ client/dbserver.js
@@ -53,7 +53,7 @@ require('dotenv').config({ path: '../server/.env' });   // Load from server .env
 
 const express = require('express');
 const { connectToDatabase } = require('./config/db.js');
-const Subscriber = require('./models/Subscriber.js');
+const Subscriber = require('../models/Subscriber');
 const cors = require('cors');
 
 const app = express();
