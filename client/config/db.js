9644d76e4520d635d990ba56c1adcbc31d02bf08
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-18 15:34:57 +0530

    Update db.js

--- client/config/db.js
+++ client/config/db.js
@@ -1,3 +1,38 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+
+// const uri = process.env.MONGODB_URI;
+
+// console.log('MONGODB_URI:', uri);
+
+// if (!uri) {
+//     throw new Error('MONGODB_URI environment variable not defined');
+// }
+
+// const connectToDatabase = async () => {
+//     try {
+//         const connection = await 
+//                 await
+//             // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//     // console.log('MongoDB connected');
+//             mongoose.connect(uri, {
+//             useNewUrlParser: true, // This is deprecated
+//             useUnifiedTopology: true, // This is also deprecated
+//         });
+//         console.log(`Connected to MongoDB at ${connection.connection.host}`);
+//     } catch (error) {
+//         console.error('Failed to connect to MongoDB:', error);
+//         process.exit(1);
+//     }
+// };
+
+// // Export the connectToDatabase function
+// module.exports = { connectToDatabase };
+
+
+
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 
@@ -12,13 +47,9 @@ if (!uri) {
 
 const connectToDatabase = async () => {
     try {
-        const connection = await 
-                await
-            // mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    // console.log('MongoDB connected');
-            mongoose.connect(uri, {
-            useNewUrlParser: true, // This is deprecated
-            useUnifiedTopology: true, // This is also deprecated
+        const connection = await mongoose.connect(uri, {
+            // useNewUrlParser: true, // This is deprecated
+            // useUnifiedTopology: true, // This is also deprecated
         });
         console.log(`Connected to MongoDB at ${connection.connection.host}`);
     } catch (error) {
@@ -29,3 +60,4 @@ const connectToDatabase = async () => {
 
 // Export the connectToDatabase function
 module.exports = { connectToDatabase };
+
