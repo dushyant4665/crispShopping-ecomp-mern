130818f17b135ae535dd37d0f9b038087b7744de
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-18 06:43:25 +0530

    Update db.js

--- client/config/db.js
+++ client/config/db.js
@@ -12,10 +12,12 @@ if (!uri) {
 
 const connectToDatabase = async () => {
     try {
-        const connection = await mongoose.connect(uri, {
+        const connection = await  
+            mongoose.connect(process.env.MONGODB_URI)
+            // mongoose.connect(uri, {
             // useNewUrlParser: true, // This is deprecated
             // useUnifiedTopology: true, // This is also deprecated
-        });
+        // });
         console.log(`Connected to MongoDB at ${connection.connection.host}`);
     } catch (error) {
         console.error('Failed to connect to MongoDB:', error);
