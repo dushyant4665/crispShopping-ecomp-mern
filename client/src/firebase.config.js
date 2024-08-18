82474f56ba29670bb36e1680cfb20112de959b2b
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-19 00:02:56 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZGvffD3djQ7QJ3hkC5yWK3dSSA0rLnt0",
@@ -13,4 +13,4 @@ const firebaseConfig = {
 };
 
  export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
