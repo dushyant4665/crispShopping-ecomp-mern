cc480d19a52adb097db0eaef5b3f11cd43f7412c
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-19 15:48:41 +0530

    Update dbserver.js

--- client/dbserver.js
+++ client/dbserver.js
@@ -89,8 +89,8 @@ app.post('/subscribe', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'));
 });
 
 const PORT = process.env.PORT || 8000;
