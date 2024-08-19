d84fe386b3a457e63bd39b583b84f7e70425c9dd
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-08-19 21:14:15 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZGvffD3djQ7QJ3hkC5yWK3dSSA0rLnt0",
-  authDomain: "ecomcrisp.firebaseapp.com",
-  projectId: "ecomcrisp",
-  storageBucket: "ecomcrisp.appspot.com",
-  messagingSenderId: "278273851374",
-  appId: "1:278273851374:web:1d2d99004cbc4add4d98ff",
-  measurementId: "G-C1DH8QBS0Q"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
  export const app = initializeApp(firebaseConfig);
