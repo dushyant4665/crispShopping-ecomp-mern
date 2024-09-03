9d7f84d7c6e0118c67f0ebbf8d63607d22d1c107
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2024-09-03 23:49:50 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,20 +1,7 @@
 // require('dotenv').config();
 
 import { initializeApp } from "firebase/app";
-// // import { getAnalytics } from "firebase/analytics";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBZGvffD3djQ7QJ3hkC5yWK3dSSA0rLnt0",
-//   authDomain: "ecomcrisp.firebaseapp.com",
-//   projectId: "ecomcrisp",
-//   storageBucket: "ecomcrisp.appspot.com",
-//   messagingSenderId: "278273851374",
-//   appId: "1:278273851374:web:1d2d99004cbc4add4d98ff",
-//   measurementId: "G-C1DH8QBS0Q"
-// };
-
-//  export const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
