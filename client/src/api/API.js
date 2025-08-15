66f7b76ecde876aba212e59ace5005af1283e623
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2025-08-15 06:15:18 -0700

    Update API.js

--- client/src/api/API.js
+++ client/src/api/API.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 export async function productsData(){
-    const product = await axios.get('https://fakestoreapiserver.reactbd.com/amazonproducts');
+    const product = await axios.get('https://fakestoreapi.com/products');
     return product;
 
     }
     // fetch('')
     // .then(res => res.json())
+
     // .then(console.log);
